bc6f347e8005c2ed1f36a933b6181bcc7ef142cf
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2024-11-05 09:07:12 +0530

    Basic Setup

--- create-app-basics/src/App.js
+++ create-app-basics/src/App.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
-  return (
-    <div>Hello, Create React App!</div>
-  )
-}
+  return <div>Hello, Create React App!</div>;
+};
 
-export default App
+export default App;
