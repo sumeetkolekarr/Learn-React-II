1dfb5e84d2dee53a19f27f16b66996e22ed26c87
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2024-11-19 19:42:29 +0530

    Learning Some React

---
+++ JS Concepts/useMap.js
@@ -0,0 +1,23 @@
+// Challenge 1
+// Use Map Method to return the squares of each number and to store it in an array
+// const nums = [1,2,3,4,5]
+// const squared = nums.map(function(item){
+//     return item * item;
+// })
+// console.log(squared);
+
+// Challenge 2
+// Return an array where the first letter of each string is capitalized
+// const names = ['sk', 'bunny', 'momo']
+// const editedNames = names.map(function(name){
+//     return name[0].toUpperCase() + name.slice(1)
+// })
+// console.log(editedNames);
+
+// Challenge 3
+// Return an array of strings that wraps start and end of strings with <p> and </p> tag
+// const pokemon = ['Bulbasaur', 'Charmander', 'Squirtle']
+// const elements = pokemon.map((pk)=>{
+//     return `<p>${pk}</p>`
+// })
+// console.log(elements);
