15f82aa721b6685e7d1c1fae30512721c4c34759
Sumeet Kolekarr <sumeetkolekarr555@gmail.com>
2024-11-25 08:01:16 +0530

    Added the Meme Generator Data File

--- dynamic-web-app/src/App.js
+++ dynamic-web-app/src/App.js
@@ -4,12 +4,48 @@ import Header from "./Header";
 import Meme from "./Meme";
 
 const App = () => {
+  //A1
+  // const thingsArray = ["Thing 1", "Thing 2"];
+  // const btnPress = () => {
+  //   thingsArray.push(`Thing ${thingsArray.length + 1}`);
+  //   document.getElementById()
+  //   console.log(thingsArray);
+  // };
+
+  // A2
+  // Props are Like the name parameter which should not be changed in the function and state in React is just like timeofday variable which changes according to the conditions
+  // function greet(name) {
+  //   const date = new Date();
+  //   const hour = date.getHours();
+  //   let timeofDay;
+  //   if (hour >= 4 && hour < 12) {
+  //     timeofDay = "Morning";
+  //   } else if (hour >= 12 && hour < 17) {
+  //     timeofDay = "Afternoon";
+  //   } else if (hour >= 17 && hour < 20) {
+  //     timeofDay = "Evening";
+  //   } else {
+  //     timeofDay = "Night";
+  //   }
+  //   return `Good ${timeofDay}, ${name}!`;
+  // }
+
+  // console.log(greet("Sumeet"));
   return (
     <>
-      <Header />
-      <Meme />
+      {/* <Header />
+      <Meme /> */}
     </>
   );
 };
+// {
+//   //A1
+// }
+// <div>
+//   <button onClick={btnPress}>Add Item</button>
+// </div>
+// {thingsArray.map((obj, idx) => {
+//   return <p key={idx}>{obj}</p>;
+// })}
 
 export default App;
--- dynamic-web-app/src/Meme.js
+++ dynamic-web-app/src/Meme.js
@@ -1,12 +1,20 @@
 import React from "react";
+import memeData from "./memeData";
 
 const Meme = () => {
+  const generateMeme = (e) => {
+    e.preventDefault();
+    let num = Math.floor(Math.random() * memeData.length);
+    console.log(memeData.data.memes[num].url);
+  };
   return (
     <main>
-      <form className="form" action="">
-          <input type="text" className="form-input" placeholder="Top Text"/>
-          <input type="text" className="form-input" placeholder="Bottom Text"/>
-          <button className="form-button">Get a new meme image 😊</button>
+      <form className="form">
+        <input type="text" className="form-input" placeholder="Top Text" />
+        <input type="text" className="form-input" placeholder="Bottom Text" />
+        <button className="form-button" onClick={generateMeme}>
+          Get a new meme image 😊
+        </button>
       </form>
     </main>
   );
